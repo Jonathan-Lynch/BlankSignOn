8fedb0179d3792542f08dc6391a6eab1557f2ec3
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-09-20 22:27:02 -0500

    modified app.module & auth.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login/login.component';
 import { RagistrationComponent } from './registration/ragistration/ragistration.component';
 import { UserProfileComponent } from './user-profile/user-profile/user-profile.component';
+import { AuthModule } from './auth/auth.module';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { UserProfileComponent } from './user-profile/user-profile/user-profile.c
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
