9022410653a0632efa67c2bfeca8786639d022c3
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-09-20 23:09:44 -0500

    finished login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formSubmit: FormGroup;
+
+  inputEmail: FormControl = new FormControl('', Validators.email);
+
+  inputPassword: FormControl = new FormControl('', Validators.compose([Validators.maxLength(20), Validators.minLength(4)]));
+
+  validLogin = true;
+
+  get inputPasswordInvalid(): boolean {
+    return !this.inputPassword.valid && this.inputPassword.touched;
+  }
+
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private router: Router) {
+
+    this.formSubmit = this.formBuilder.group({
+      inputEmail: this.inputEmail,
+      inputPassword: this.inputPassword
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  submit(): void {
+    console.log(this.formSubmit.value);
+    this.validLogin = this.authService.login(this.formSubmit.controls.inputEmail.value,
+      this.formSubmit.controls.inputPassword.value);
+
+    if (this.validLogin) {
+        this.router.navigate(['/userprofile']);
+      }
   }
 
 }
