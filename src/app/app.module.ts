78e4d5bd71defe94f111bbe1e9120f6b2d75fba6
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-09-20 23:28:00 -0500

    I think i finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,15 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login/login.component';
-import { RagistrationComponent } from './registration/ragistration/ragistration.component';
-import { UserProfileComponent } from './user-profile/user-profile/user-profile.component';
-import { AuthModule } from './auth/auth.module';
+import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
-
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RagistrationComponent,
     UserProfileComponent,
     RegistrationComponent
   ],
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthModule } from './auth.module';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: AuthModule
 })
 export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService,
+              private router: Router) {}
   canActivate(
-    route: ActivatedRouteSnapshot,
+    next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+      if (this.authService.loggedIn) {
+        return true;
+      }
+      this.router.navigate(['/login']);
+
+      return false;
   }
-  
+
 }
